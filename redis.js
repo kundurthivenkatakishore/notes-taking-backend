8ab9488a09acfe388402e70c6bb1f3380365adb4
kundurthivenkatakishore <kundurthivenkatakishore@gmail.com>
2023-11-19 21:09:03 +0530

    live redis url added

--- redis.js
+++ redis.js
@@ -1,7 +1,12 @@
 import redis from "redis";
+import dotenv from "dotenv"
+
+dotenv.config()
 
 const redisClient = () => {
-    return redis.createClient()
+    return redis.createClient({
+        url: process.env.REDIS_URL
+    })
 }
 
 const client = redisClient();
